d8671b18dfc708a190b106c760fd0fa53c4c4b40
José Gabriel do Nascimento <gjose4587@gmail.com>
2026-01-02 22:23:29 -0300

    v0.3.0
    Seeding Service:
    
    - Adicionado seedingService (Método que preenche o banco com informações ao executar o projeto/atualizar base de dados).

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -7,19 +7,25 @@ var builder = WebApplication.CreateBuilder(args);
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string `DefaultConnection` not found.");
 
-builder.Services.AddDbContext<SalesWebMvcContext>(options =>
-    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+builder.Services.AddDbContext<SalesWebMvcContext>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+builder.Services.AddScoped<SeedingService>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+// Executa o seeding apenas em Development
+if (app.Environment.IsDevelopment())
+{
+    using var scope = app.Services.CreateScope();
+    var seedingService = scope.ServiceProvider.GetRequiredService<SeedingService>();
+    seedingService.Seed();
+}
+else
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
